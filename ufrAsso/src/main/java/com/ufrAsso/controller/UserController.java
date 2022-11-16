b7374863c406a087bae1cde9d3b13a35280628e0
Miroredge <Maximilien.poncet19@gmail.com>
2022-11-16 14:29:39 +0100

    Mapping User_Controller_step_1

--- ufrAsso/src/main/java/com/ufrAsso/controller/UserController.java
+++ ufrAsso/src/main/java/com/ufrAsso/controller/UserController.java
@@ -34,4 +34,47 @@ public class UserController {
         User newUser = userRepo.save(user);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
+
+    @GetMapping("api/users/{id}") // Get a user by id
+    public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
+        Optional<User> user = userRepo.findById(id);
+        if (user.isPresent()) {
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("api/users/{id}") // Update a user by id
+    public ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user) {
+        Optional<User> userData = userRepo.findById(id);
+        if (userData.isPresent()) {
+            User _user = userData.get();
+            _user.setStudent_number(user.getStudent_number());
+            _user.setProfile_picture(user.getProfile_picture());
+            _user.setFirst_name(user.getFirst_name());
+            _user.setLast_name(user.getLast_name());
+            _user.setSex(user.getSex());
+            _user.setEmail(user.getEmail());
+            _user.setPhone_number(user.getPhone_number());
+            _user.setPhone_book(user.getPhone_book());
+            _user.setPassword(user.getPassword());
+            _user.setTemporary_password(user.getTemporary_password());
+            _user.setNotification(user.getNotification());
+            return new ResponseEntity<>(userRepo.save(_user), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("api/users/{id}") // Delete a user by id
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable("id") long id) {
+        try {
+            userRepo.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
